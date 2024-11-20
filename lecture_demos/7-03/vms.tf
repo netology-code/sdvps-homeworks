3e07c9739d4452f4c3f5c7e1a84f6f454ad9c413
udjin <udjin10@gmail.com>
2024-11-20 18:30:41 +0300

    add wrong name vm

--- lecture_demos/7-03/vms.tf
+++ lecture_demos/7-03/vms.tf
@@ -109,6 +109,41 @@ resource "yandex_compute_instance" "web_b" {
   }
 }
 
+resource "yandex_compute_instance" "wrong_b" {
+  name        = "wrong-hostname" #Имя ВМ в облачной консоли
+  platform_id = "standard-v3"
+  zone        = "ru-central1-b" #зона ВМ должна совпадать с зоной subnet!!!
+
+  resources {
+    cores         = var.test.cores
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_b.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+
+  }
+}
+
+
 resource "local_file" "inventory" {
   content  = <<-XYZ
   [bastion]
